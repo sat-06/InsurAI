a2e254c3710eba302516ded7c364f20d8ccd7850
Satyam Kulkarni <kulkarnisatyam666@gmail.com>
2026-07-03 00:34:25 +0530

    Backend api test

--- backend/routes/predict.py
+++ backend/routes/predict.py
@@ -1,62 +1,32 @@
-import joblib
-import pandas as pd
 from fastapi import APIRouter
+import pandas as pd
+import joblib
 
 router = APIRouter()
 
 # Load trained model
 model = joblib.load("ml/saved_models/insurance_model.pkl")
 
-# Load feature order
-feature_names = joblib.load("ml/saved_models/feature_names.pkl")
-
 
 @router.post("/predict")
 def predict(data: dict):
 
+    # Encode categorical variables
     sex = 1 if data["sex"].lower() == "male" else 0
     smoker = 1 if data["smoker"].lower() == "yes" else 0
 
-    input_data = pd.DataFrame([{
-    "age": data["age"],
-    "sex": sex,
-    "bmi": data["bmi"],
-    "children": data["children"],
-    "smoker": smoker,
+    # Create input dataframe
+    input_data = pd.DataFrame([
+        {
+            "age": data["age"],
+            "sex": sex,
+            "bmi": data["bmi"],
+            "children": data["children"],
+            "smoker": smoker
+        }
+    ])
 
-    "region_northwest": 0,
-    "region_southeast": 0,
-    "region_southwest": 0,
-
-    "bmi_category_Normal": 0,
-    "bmi_category_Overweight": 0,
-    "bmi_category_Obese": 0
-}])
-    
-        # Region Encoding
-    if data["region"].lower() == "northwest":
-        input_data["region_northwest"] = 1
-
-    elif data["region"].lower() == "southeast":
-        input_data["region_southeast"] = 1
-
-    elif data["region"].lower() == "southwest":
-        input_data["region_southwest"] = 1
-
-
-    # BMI Category Encoding
-    if data["bmi"] < 25:
-        input_data["bmi_category_Normal"] = 1
-
-    elif data["bmi"] < 30:
-        input_data["bmi_category_Overweight"] = 1
-
-    else:
-        input_data["bmi_category_Obese"] = 1
-
-    # Ensure correct column order
-    input_data = input_data[feature_names]
-
+    # Predict
     prediction = model.predict(input_data)[0]
 
     return {
